e58e9b5d99bdf5d223e1da500560e5a3ef66aaab
Yusuf Karim <ykarim250@gmail.com>
2019-01-05 12:28:58 -0500

    🔥 Removed Eventbrite api constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,16 +1,13 @@
-export const IFF_ORG_ID = '79179389909';
-export const EVENTBRITE_API_KEY = '';
-
 export const MOVIE_QUERY = {
     BASE_URL: "https://api.themoviedb.org/3/",
     IMAGE_URL: "http://image.tmdb.org/t/p/w185",
     API_KEY: "api_key=151dfa1b4c6a83a02970c0c6612615b3",
     SEARCH_QUERY: "search/movie?query=",
-    
-}
+
+};
 export const FILMS = [
     "Vox Lux", "Wild Nights With Emily", "Forrest Gump",
-]
+];
 
 export const MOVIE_FIELDS = {
     MAIN_TITLE: "Movie Database App",
@@ -22,10 +19,9 @@ export const MOVIE_FIELDS = {
     POPULARITY: "Popularity: ",
     STATUS: "Status: ",
     RATINGS: "Ratings: ",
-    POPULARITY : "Popularity: ",
     BUDGET: "Budget: ",
     REVENUE: "Revenue: ",
     RUNTIME: "Runtime: ",
     OVERVIEW: "Overview: ",
     MSG: "This field is required."
-}
+};
