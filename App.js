68302f5193fecc0c13ac8a5d98c3eb138848371b
Yusuf Karim <ykarim250@gmail.com>
2019-01-09 00:26:07 -0500

    ✨ Persist Settings

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {Provider as StoreProvider} from 'react-redux';
+import {PersistGate} from 'redux-persist/integration/react'
 import Info from './src/components/info/Info';
 import Schedule from './src/components/schedule/Schedule';
 import EventDetails from './src/components/schedule/EventDetails';
@@ -9,7 +10,7 @@ import {createAppContainer, createStackNavigator} from 'react-navigation';
 import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
 import FATabBarIcon from "./src/components/shared/FATabBarIcon";
 import MaterialTabBarIcon from "./src/components/shared/MaterialTabBarIcon";
-import {store} from "./src/redux/store";
+import {persistor, store} from "./src/redux/store";
 
 const InfoNavigator = createStackNavigator(
     {
@@ -87,13 +88,16 @@ const AppNavigator = createMaterialBottomTabNavigator(
 
 const AppContainer = createAppContainer(AppNavigator);
 
+//TODO: Return AppLoading if PersistGate incomplete
 export default class App extends React.Component {
   render() {
     return (
         <StoreProvider store={store}>
-            <PaperProvider>
-                <AppContainer/>
-            </PaperProvider>
+            <PersistGate persistor={persistor}>
+                <PaperProvider>
+                    <AppContainer/>
+                </PaperProvider>
+            </PersistGate>
         </StoreProvider>
     );
   }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,14 @@
 import {createStore} from 'redux';
+import {persistReducer, persistStore} from 'redux-persist';
 import {rootReducer} from "./reducers";
+import storage from 'redux-persist/lib/storage';
 
-export const store = createStore(rootReducer);
+const persistConfig = {
+    key: '@IvyFilmFestivalStore:',
+    storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
